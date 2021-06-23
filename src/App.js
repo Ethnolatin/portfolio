b085efe12a440dd92cac995b741a90a6019a61a1
Ethnolatin <duchesne.fred@gmail.com>
2021-06-23 10:47:42 +0200

    display a wip page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="mon portrait" />
-        
+        <h1>Bonjour !</h1>
+        <p>Je m'appelle</p>
+        <h2>Frédéric Duchesne</h2>
+        <p>Je suis</p>
+        <h3>développeur web</h3>
+        <p>et ici vous trouverez bientôt</p>
+        <h3>mon portfolio</h3>
       </header>
     </div>
   );
