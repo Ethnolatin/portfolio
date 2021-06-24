fd6dbd1faeeaa6fbed65a21a8f1125e5ab1ed48a
Ethnolatin <duchesne.fred@gmail.com>
2021-06-24 10:38:50 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './styles/css/style.css';
-import logo from './images/photo_id-220.jpg'
+import { Header } from './components/header'
+// import { Skills } from './components/skills'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="mon portrait" />
-        <h1>Bonjour !</h1>
-        <p>Je m'appelle</p>
-        <h2>Frédéric Duchesne</h2>
-        <p>Je suis</p>
-        <h3>développeur web</h3>
-        <p>et ici vous trouverez bientôt</p>
-        <h3>mon portfolio</h3>
-      </header>
-    </div>
+    <main>
+      <Header />
+      {/* <Skills /> */}
+    </main>
+    
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import logo from '../images/photo_id-220.jpg'
+
+export const Header = () => {
+    return (
+        <section>
+            <img src={logo} className="app-logo" alt="mon portrait" />
+            <h1>Frédéric Duchesne</h1>
+            <h2>Développeur Web</h2>
+            <p>Bonjour, je m'appelle <strong>Frédéric Duchesne</strong>, je suis <strong>développeur web</strong>, plutôt orienté vers les <strong>projets internationaux</strong> avec une forte préférence pour l'<strong>Amérique latine</strong>, disponible pour une collaboration en tant que <strong>salarié ou freelance</strong>, en <strong>télétravail</strong>.</p>
+        </section>
+    )
+}
+    
