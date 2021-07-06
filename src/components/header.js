2d16899edbfb59dc490678cd695f3b97ec71aab7
Ethnolatin <duchesne.fred@gmail.com>
2021-07-06 12:44:46 +0200

    add a background image to the header

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,15 @@ import { More } from './more'
 
 export const Header = () => {
     return (
-        <section>
-            <img src={myFace} className="portrait" alt="mon portrait" />
-            <h1>Frédéric Duchesne</h1>
-            <h2>Développeur Web</h2>
-            <p>Bonjour, je m'appelle <strong>Frédéric Duchesne</strong>, je suis <strong>développeur web</strong>, plutôt orienté vers les <strong>projets internationaux</strong> avec une forte préférence pour l'<strong>Amérique latine</strong>, disponible pour une collaboration en <strong>télétravail</strong> en tant que <strong>salarié ou freelance</strong>.</p>
-            <More />
-        </section>
+        <header>
+            <section>
+                <img src={myFace} className="portrait" alt="mon portrait" />
+                <h1>Frédéric Duchesne</h1>
+                <h2>Développeur Web</h2>
+                <p>Bonjour, je m'appelle <strong>Frédéric Duchesne</strong>, je suis <strong>développeur web</strong>, plutôt orienté vers les <strong>projets internationaux</strong> avec une forte préférence pour l'<strong>Amérique latine</strong>, disponible pour une collaboration en <strong>télétravail</strong> en tant que <strong>salarié ou freelance</strong>.</p>
+                <More />
+            </section>
+        </header>
     )
 }
     
