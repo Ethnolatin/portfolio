b98e580be41ea1aa082cb3c13f430f20f902a8b6
Ethnolatin <duchesne.fred@gmail.com>
2021-07-28 11:20:54 +0200

    fix a bug that would display up-arrow over modals

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ export const Header = () => {
                 <img src={myFace} className="portrait" alt="mon portrait" />
                 <h1>Frédéric Duchesne</h1>
                 <h2>Développeur Web</h2>
-                <p>Bonjour, je m'appelle <strong>Frédéric Duchesne</strong>, je suis <strong>développeur web</strong>, avec une bonne expérience du travail dans un <strong>contexte international</strong>, disponible pour une collaboration en <strong>télétravail</strong> en tant que <strong>salarié ou freelance</strong>.</p>
+                <p>Bonjour, je m'appelle <strong>Frédéric Duchesne</strong>, je suis <strong>développeur web</strong>, avec une bonne <strong>expérience de l'entreprise</strong> et du travail dans un <strong>contexte international</strong>, disponible pour une collaboration en <strong>télétravail</strong> en tant que <strong>salarié ou freelance</strong>.</p>
                 <HeaderButtons />
             </section>
             <img src={headerRight} alt="" />
